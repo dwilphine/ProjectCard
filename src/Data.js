a00e645ac0e118ed0394be46474647b9eb446f60
dwilphine <delianebethyna17@gmail.comgit>
2025-06-20 16:57:21 -0400

    first commit

---
+++ src/Data.js
@@ -0,0 +1,164 @@
+let shopItemsData = [
+  {
+    id: "adcb",
+    name: "kòtèl lakay",
+    price: 75,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "kremas kokoye",
+    price: 90,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "kremas grenadia",
+    price: 85,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mini manba",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+
+
+  {
+    id: "thyt",
+    name: "Mini_manba",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpg",
+  },
+
+{
+    id: "fhcbcv",
+    name: "Makòs lakay",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.jpg",
+  },
+
+  {
+    id: "cbcv",
+    name: "grenn kakao",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.jpg",
+  },
+
+  {
+    id: "dbfjcv",
+    name: "konfiti + Makòs",
+    price: 150,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpg",
+  },
+
+  {
+    id: "mnfsea",
+    name: "Mini kòktèl",
+    price: 80,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-9.jpg",
+  },
+
+  {
+    id: "zxcvgfds",
+    name: "Miklkre",
+    price: 95,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-11.jpg",
+  },
+
+{
+    id: "tokjng",
+    name: "bon manba",
+    price: 68,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-12.jpg",
+  },
+
+  {
+    id: "tyhfdre",
+    name: "package manba",
+    price: 260,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-13.jpg",
+  },
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
